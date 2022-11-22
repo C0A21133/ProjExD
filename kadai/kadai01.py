451aaf7e1a077e01da499fee2cc839539e79a1d2
YamashitaRyutaro <ryu.1522.1a.1c.12.10@gmail.com>
2022-11-22 14:22:27 +0900

    second commit

--- kadai/kadai01.py
+++ kadai/kadai01.py
@@ -4,11 +4,22 @@
 import random
 import datetime
 
+#グローバル変数
+Target_Length = 10 #対象文字数
+Missing_Length = 2 #欠陥文字数
+Max_Count = 10 #最大試行回数
+Alphabet_List = [chr(ord("A")+i) for i in range(26)] #A～Zまでのアルファベットを持つリスト
 
+def main():
+    select_target_alphabet()
+    pass
+
+def select_target_alphabet():
+    tmp = []
+    for i in range(Target_Length):
+        tmp.append(Alphabet_List.pop(random.randint(0,25-i)))
+    print(tmp)
 
 if __name__ == "__main__":
-    target_length = 5 #対象文字数
-    missing_length = 2 #欠陥文字数
-    count = 0 #試行回数
-    
+    main()
     pass
