d6292518b48baf337b2c750385fe707023ff3f6b
YamashitaRyutaro <ryu.1522.1a.1c.12.10@gmail.com>
2022-11-22 14:21:47 +0900

    six commit

--- ex01/quiz.py
+++ ex01/quiz.py
@@ -1,6 +1,13 @@
 import random
 import datetime
 
+Quiz_Data = [
+        ("サザエの旦那の名前は？", ("ますお", "マスオ")),
+        ("カツオの妹の名前は？", ("ワカメ", "わかめ")),
+        ("タラオはカツオから見てどんな関係？", ("甥", "甥っ子", "おいっこ"))
+        #( 問題文 , （回答）)
+        ]
+
 def main():
     quiz_num = random.randint(0, 2) #出題する問題をランダムに決める
     start_time = datetime.datetime.now() #問題出題時の時刻を記録
@@ -8,15 +15,15 @@ def main():
     kaitou(quiz_num, start_time)
 
 def shutudai(quiz_num):
-    print(quiz_data[quiz_num][0])
+    print(Quiz_Data[quiz_num][0])
     
 def kaitou(quiz_num, start_time):
     your_answer = input("答えは？:")
-    num = len(quiz_data[quiz_num][1])
+    num = len(Quiz_Data[quiz_num][1])
     finish_time = datetime.datetime.now() #問題回答時の時刻を記録
     
     for i in range(num):
-        if your_answer == quiz_data[quiz_num][1][i]:
+        if your_answer == Quiz_Data[quiz_num][1][i]:
             your_answer = True
             print("正解!")
     if your_answer != True:
@@ -27,12 +34,5 @@ def kaitou(quiz_num, start_time):
 
 
 if __name__ == "__main__":
-    quiz_data = [
-        ("サザエの旦那の名前は？", ("ますお", "マスオ")),
-        ("カツオの妹の名前は？", ("ワカメ", "わかめ")),
-        ("タラオはカツオから見てどんな関係？", ("甥", "甥っ子", "おいっこ"))
-        #( 問題文 , （回答）)
-        ]
-    
     main()
     
