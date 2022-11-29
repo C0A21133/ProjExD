f801c80537c600ee533d1bd3f4956fd8d49cd3aa
YamashitaRyutaro <ryu.1522.1a.1c.12.10@gmail.com>
2022-11-29 14:03:09 +0900

    change name

---
+++ ex02/calc.py
@@ -0,0 +1,38 @@
+#電卓を表示するプログラム
+import tkinter as tk
+import math
+
+#電卓に表示するボタン
+BUTTON_LIST = [
+        ["9", "8", "7"],
+        ["6", "5", "4"],
+        ["3", "2", "1"],
+        ["0"]
+    ]
+
+#ウインドウのサイズ
+WINDOWS_SIZE = (300, 500)
+
+#電卓のGuiの設定
+class CalcGui(object):
+    def __init__(self, app=None):
+        app.title('calculator') #ウインドウのタイトル
+        app.geometry(f"{WINDOWS_SIZE[0]}x{WINDOWS_SIZE[1]}") #ウインドウのサイズ
+        
+        #ボタンをBUTTON_LISTから取得し配置
+        for y, row in enumerate(BUTTON_LIST, 1):
+            for x, num in enumerate(row):
+                button = tk.Button(app, text=num, width=4, height=2, font=("", 30))
+                button.grid(row = y, column=x)
+    
+
+    
+
+def main():
+    app = tk.Tk()
+    CalcGui(app)
+    app.mainloop()
+    
+    
+if __name__ == "__main__":
+    main()
