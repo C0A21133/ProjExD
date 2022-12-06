ccd04542f4820fa3e83c19cdce6803cfd1d00784
YamashitaRyutaro <ryu.1522.1a.1c.12.10@gmail.com>
2022-12-06 14:08:49 +0900

    create canvas

--- ex03/maze.py
+++ ex03/maze.py
@@ -1,3 +1,23 @@
 #
-import tkinter
+import tkinter as tk
+import maze_maker
 
+#ウインドウのサイズ
+WINDOWS_SIZE = (1500, 900) #(X,Y)
+
+    
+class Application(tk.Frame):
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.master.title('maze') #ウインドウのタイトル
+        self.master.geometry(f"{WINDOWS_SIZE[0]}x{WINDOWS_SIZE[1]}") #ウインドウのサイズ
+        
+        canvas = tk.Canvas(self.master, width=1500, height=900, bg = "black")
+        canvas.pack()
+    
+    
+    
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = Application(master=root)
+    app.mainloop()
