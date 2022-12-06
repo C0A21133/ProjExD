e6aae45817ae46e2943301bf405bd35037520277
YamashitaRyutaro <ryu.1522.1a.1c.12.10@gmail.com>
2022-12-06 14:41:17 +0900

    add image

--- ex03/maze.py
+++ ex03/maze.py
@@ -1,19 +1,35 @@
 #
 import tkinter as tk
+from PIL import ImageTk
 import maze_maker
 
 #ウインドウのサイズ
 WINDOWS_SIZE = (1500, 900) #(X,Y)
 
-    
+
+
 class Application(tk.Frame):
     def __init__(self, master=None):
         super().__init__(master)
-        self.master.title('maze') #ウインドウのタイトル
+        self.master.title('迷えるこうかとん') #ウインドウのタイトル
         self.master.geometry(f"{WINDOWS_SIZE[0]}x{WINDOWS_SIZE[1]}") #ウインドウのサイズ
         
-        canvas = tk.Canvas(self.master, width=1500, height=900, bg = "black")
-        canvas.pack()
+        self.canvas = tk.Canvas(self.master, width=WINDOWS_SIZE[0], height=WINDOWS_SIZE[1], bg = "black")
+        self.canvas.pack()
+        
+        self.kou = Koukaton(file=ImageTk.PhotoImage(file="fig/0.png"), cx=300, cy=400)
+        
+        self.canvas.create_image(self.kou.cx, self.kou.cy, image=self.kou.file)
+        
+        
+        
+class Koukaton():
+    def __init__(self, file, cx, cy) -> None:
+        self.file = file
+        self.cx = cx
+        self.cy = cy
+    
+    
     
     
     
