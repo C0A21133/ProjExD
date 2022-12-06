54c61c926a43e331821e983a2a32626488a5f033
YamashitaRyutaro <ryu.1522.1a.1c.12.10@gmail.com>
2022-12-06 15:11:26 +0900

    fix

--- ex03/maze.py
+++ ex03/maze.py
@@ -6,42 +6,45 @@ from PIL import ImageTk
 
 #ウインドウのサイズ
 WINDOWS_SIZE = (1500, 900) #(X,Y)
-
-
-
-class Application(tk.Frame):
-    def __init__(self, master=None):
-        super().__init__(master)
-        self.master.title('迷えるこうかとん') #ウインドウのタイトル
-        self.master.geometry(f"{WINDOWS_SIZE[0]}x{WINDOWS_SIZE[1]}") #ウインドウのサイズ
-        
-        self.canvas = tk.Canvas(self.master, width=WINDOWS_SIZE[0], height=WINDOWS_SIZE[1], bg = "black")
-        self.canvas.pack()
-        
-        self.kou = Koukaton(file=ImageTk.PhotoImage(file="fig/0.png"), cx=300, cy=400)
-        
-        self.canvas.create_image(self.kou.cx, self.kou.cy, image=self.kou.file)
-        
-        
         
 class Koukaton():
-    def __init__(self, file, cx, cy) -> None:
-        self.file = file
+    def __init__(self, cx, cy, image):
         self.cx = cx
         self.cy = cy
+        self.image = ImageTk.PhotoImage(file=image) 
+
+    
     
 def key_down(event):
+    global key
     key = event.keysym
     tkm.showinfo(f"{key}", f"{key}")
     
 def key_up(event):
-    pass
+    global key
+    key = ""
+    
+def main_peoc():
+    global key
+    if key == "Up":
+        pass
     
     
 if __name__ == "__main__":
     key = ""
+    
     root = tk.Tk()
+    koukaton = Koukaton(cx=300, cy=400, image="fig/0.png")
+    
     root.bind("<KeyPress>", key_down)
     root.bind("<KeyRelease>", key_up)
-    app = Application(master=root)
-    app.mainloop()
+    
+    root.title('迷えるこうかとん') #ウインドウのタイトル
+    root.geometry(f"{WINDOWS_SIZE[0]}x{WINDOWS_SIZE[1]}") #ウインドウのサイズ    
+    canvas = tk.Canvas(root, width=WINDOWS_SIZE[0], height=WINDOWS_SIZE[1], bg = "black")
+    canvas.pack()
+    
+    canvas.create_image(koukaton.cx, koukaton.cy, image=koukaton.image)
+    
+    
+    root.mainloop()
