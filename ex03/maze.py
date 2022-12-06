d0c54bb0264f415ea2ed1db2866048251ac82b8a
YamashitaRyutaro <ryu.1522.1a.1c.12.10@gmail.com>
2022-12-06 15:29:56 +0900

    add main_proc

--- ex03/maze.py
+++ ex03/maze.py
@@ -18,17 +18,34 @@ class Koukaton():
 def key_down(event):
     global key
     key = event.keysym
-    tkm.showinfo(f"{key}", f"{key}")
+    #tkm.showinfo(f"{key}", f"{key}")
     
 def key_up(event):
     global key
     key = ""
     
 def main_peoc():
-    global key
+    global key, koukaton
     if key == "Up":
-        pass
+        koukaton.cy += 20
+    elif key == "Down":
+        koukaton.cy += -20
+    elif key == "Right":
+        koukaton.cx += 20
+    elif key == "Left":
+        koukaton.cx += -20
     
+    #画像の座標の取得
+    points = canvas.coords(image)
+    print(points)
+    
+    #座標の修正
+    points = [koukaton.cx, koukaton.cy]
+    
+    #画像の移動
+    points = canvas.coords(image, points)
+    
+    root.after(60, main_peoc)
     
 if __name__ == "__main__":
     key = ""
@@ -39,12 +56,15 @@ if __name__ == "__main__":
     root.bind("<KeyPress>", key_down)
     root.bind("<KeyRelease>", key_up)
     
+    #キャンバスの作成
     root.title('迷えるこうかとん') #ウインドウのタイトル
     root.geometry(f"{WINDOWS_SIZE[0]}x{WINDOWS_SIZE[1]}") #ウインドウのサイズ    
     canvas = tk.Canvas(root, width=WINDOWS_SIZE[0], height=WINDOWS_SIZE[1], bg = "black")
     canvas.pack()
     
-    canvas.create_image(koukaton.cx, koukaton.cy, image=koukaton.image)
-    
-    
+    #こうかとん の表示
+    image = canvas.create_image(koukaton.cx, koukaton.cy, image=koukaton.image)
+    #処理
+    main_peoc()
+        
     root.mainloop()
