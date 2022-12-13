54808793e9ed2b89b2080c083dafe0b8121d7fe3
YamashitaRyutaro <ryu.1522.1a.1c.12.10@gmail.com>
2022-12-13 12:35:07 +0900

    add 3

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -2,7 +2,10 @@ import pygame as pg
 import sys
 import os
 
-
+class Koukaton:
+    def __init__(self, im_pass, pos) -> None:
+        self.im_pass = im_pass
+        self.pos = pos
 
 def main():
     os.chdir(os.path.dirname(__file__))
@@ -18,9 +21,17 @@ def main():
     bg_image = pg.image.load("pg_bg.jpg")
     rect_bg = bg_image.get_rect()
     
+    koukaton = Koukaton(im_pass="../fig/0.png", pos=(900, 400))
+    kou_image = pg.image.load(koukaton.im_pass)
+    kou_image = pg.transform.rotozoom(kou_image, 0, 2.0)
+    rect_kou = kou_image.get_rect()
+    rect_kou.center = koukaton.pos[0], koukaton.pos[1]
+    
+    
     while (1):
         pg.display.update()
         scrn_sfc.blit(bg_image, rect_bg)
+        scrn_sfc.blit(kou_image, rect_kou)
         
         for event in pg.event.get():
             if event.type == pg.QUIT:
