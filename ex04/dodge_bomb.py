77858ddccf65f4a0ffce441e876993fda9058598
YamashitaRyutaro <ryu.1522.1a.1c.12.10@gmail.com>
2022-12-13 14:57:21 +0900

    7

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -1,6 +1,7 @@
 import pygame as pg
 import random
 import sys
+import copy
 import os
 
 WINDOW_SIZE = (1600, 900)
@@ -10,9 +11,13 @@ class Image:
         self.im_pass = im_pass
         self.pos = pos
         
+class Koukaton(Image):
+    def __init__(self, im_pass, pos, speed=2) -> None:
+        super().__init__(im_pass, pos)
+        self.speed = speed
 
 class Bomb:
-    def __init__(self, rad= 10, color = "red", speed=1) -> None:
+    def __init__(self, rad= 10, color = "red", speed=[1, 1]) -> None:
         x = random.randint(0, WINDOW_SIZE[0])
         y = random.randint(0, WINDOW_SIZE[1])
         self.pos = [x, y]
@@ -21,8 +26,13 @@ class Bomb:
         self.speed = speed
         
     def move(self):
-        self.pos[0] += self.speed
-        self.pos[1] += self.speed
+        for i in range(2):
+            # i == 0 のとき x 軸の計算
+            # i == 1 のとき y 軸の計算
+            self.pos[i] += self.speed[i]
+            if 0 >= self.pos[i] or self.pos[i] + self.rad >= WINDOW_SIZE[i]:
+                self.speed[i] *= -1
+            
 
 def main():
     os.chdir(os.path.dirname(__file__))
@@ -38,32 +48,41 @@ def main():
     bg_image = pg.image.load("pg_bg.jpg")
     rect_bg = bg_image.get_rect()
     
-    koukaton = Image(im_pass="../fig/0.png", pos=(900, 400))
+    koukaton = Koukaton(im_pass="../fig/0.png", pos=(900, 400))
     koukaton_image = pg.image.load(koukaton.im_pass)
     koukaton_image = pg.transform.rotozoom(koukaton_image, 0, 2.0)
     koukaton_rect = koukaton_image.get_rect()
     koukaton_rect.center = koukaton.pos[0], koukaton.pos[1]
-    
+
     bomb = Bomb()
     
+    print(type(koukaton_rect))
     
     while (1):
-        pg.display.update()
         scrn_sfc.blit(bg_image, rect_bg)
         scrn_sfc.blit(koukaton_image, koukaton_rect)
         
         bomb.move()
         bomb_circle = pg.draw.circle(scrn_sfc, (bomb.color), bomb.pos, bomb.rad)
         
+        before_koukaton_rect = copy.deepcopy(koukaton_rect) #こうかとん の移動前の座標
         pressed = pg.key.get_pressed()
         if pressed[pg.K_LEFT]:
-            koukaton_rect.move_ip(-1, 0)
+            koukaton_rect.move_ip(-1 * koukaton.speed, 0)
         if pressed[pg.K_RIGHT]:
-            koukaton_rect.move_ip(1, 0)
+            koukaton_rect.move_ip(koukaton.speed, 0)
         if pressed[pg.K_UP]:
-            koukaton_rect.move_ip(0, -1)
+            koukaton_rect.move_ip(0, -1 * koukaton.speed)
         if pressed[pg.K_DOWN]:
-            koukaton_rect.move_ip(0, 1)
+            koukaton_rect.move_ip(0, koukaton.speed)
+        
+        #こうかとん が画面外に出たとき、元の位置に戻す、
+        for i in range(2):
+            # i == 0 のとき x座標が範囲外
+            # i == 1 のとき y座標が範囲外
+            if 0 >= koukaton_rect[i] or koukaton_rect[i] + koukaton_rect[2+i]>= WINDOW_SIZE[i]:
+                print(before_koukaton_rect)
+                koukaton_rect = before_koukaton_rect
         
         for event in pg.event.get():
             if event.type == pg.QUIT:
