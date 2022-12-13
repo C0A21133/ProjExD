65fb2a6f70fd656bdcd75d508b202b8ec4404f93
YamashitaRyutaro <ryu.1522.1a.1c.12.10@gmail.com>
2022-12-13 11:34:55 +0900

    add 2

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -1,15 +1,36 @@
 import pygame as pg
 import sys
+import os
 
 def main():
+    os.chdir(os.path.dirname(__file__))
+    print("pass"+os.getcwd())
+    pg.init()
     pg.display.set_caption("逃げろ！こうかとん")
     scrn_sfc = pg.display.set_mode((1600, 900))
-
-
+    screen = pg.display.get_surface()
+    
+    clock = pg.time.Clock()
+    clock.tick(1000)
+    
+    bg_image = pg.image.load("pg_bg.jpg")
+    rect_bg = bg_image.get_rect()
+    
+    while (1):
+        pg.display.update()
+        scrn_sfc.blit(bg_image, rect_bg)
+        
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                pg.quit()
+                sys.exit()
+                
+            if event.type == pg.KEYDOWN:
+                if event.key == pg.K_ESCAPE:
+                    pg.quit()
+                    sys.exit()
 
 
 if __name__ == "__main__":
-    pg.init()
     main()
-    pg.quit()
-    sys.exit()
+    
